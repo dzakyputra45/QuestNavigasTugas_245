7ba81a646e80d4a13b2ce4d6b2f2be3b204c7102
dzakyputra45 <dzakypratama45@gmail.com>
2025-11-06 22:17:28 +0700

    Tambahkan Welcome Screen untuk awal navigasi dan button submit untuk ke halaman selanjutnya

---
+++ app/src/main/java/com/example/tugasnavigation/view/WelcomeScreen.kt
@@ -0,0 +1,50 @@
+package com.example.tugasnavigation.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.tugasnavigation.R
+
+@Composable
+fun WelcomeScreen(navController: NavHostController) {
+    Surface(modifier = Modifier.fillMaxSize()) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Selamat Datang", fontSize = 28.sp, fontWeight = FontWeight.Bold)
+
+            Spacer(modifier = Modifier.height(24.dp))
+
+            Image(
+                painter = painterResource(id = R.drawable.cardlist_logo),
+                contentDescription = "Logo",
+                modifier = Modifier.size(150.dp)
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Text("Dzaky Putra Pratama", fontSize = 18.sp)
+            Text("20250140245", fontSize = 14.sp,
+                color = MaterialTheme.colorScheme.secondary)
+
+            Spacer(modifier = Modifier.height(32.dp))
+
+            Button(
+                onClick = { navController.navigate("listPeserta") },
+                modifier = Modifier.fillMaxWidth(0.7f).height(50.dp)
+            ) {
+                Text("Submit")
+            }
+        }
+    }
+}
